4b148667c56a2d0ed3a3ced6e25f28ff88c5906b
qyuan7 <qyuan@ic.ac.uk>
2020-05-20 17:47:45 +0100

    Added preprocess layer for molecular fps

--- models/gcn_models.py
+++ models/gcn_models.py
@@ -9,8 +9,10 @@ class gcn(nn.Module):
 
     def __init__(self, input_dim, hidden_dim, dropout=0.5, n_class=1):
         super().__init__()
-        self.gc1 = GraphConvolution(input_dim, hidden_dim)
-        self.gc2 = GraphConvolution(hidden_dim, n_class)
+        self.pre = nn.Linear(input_dim, 64)
+        self.gc1 = GraphConvolution(64, hidden_dim)
+        self.gc2 = GraphConvolution(hidden_dim, hidden_dim)
+        self.gc3 = GraphConvolution(hidden_dim, n_class)
         self.dropout = dropout
         self.n_class = n_class
 
@@ -23,17 +25,19 @@ class gcn(nn.Module):
             adj_hat = D_sqrt * adj * D_sqrt
             adj_hat = torch.from_numpy(adj_hat).float()
         else:
-            adj_hat = adj
+            adj_hat = torch.from_numpy(adj).float() 
         adj_hat = adj_hat.clone().detach().requires_grad_(True)
         return adj_hat
 
     def forward(self, x, adj):
         adj = self.init_adj(adj)
+        x = F.relu(self.pre(x))
         x = F.relu(self.gc1(x, adj))
         x = F.dropout(x, self.dropout, self.training)
-        x = self.gc2(x, adj)
-
+        #x = F.relu(self.gc2(x, adj))
+        #x = F.dropout(x, self.dropout, self.training)
+        x = self.gc3(x,adj)
         x = torch.sum(x, 1)
-        if self.n_class > 1:
-            x = F.log_softmax(x, dim=1)
+        #if self.n_class > 1:
+        #    x = F.log_softmax(x, dim=1)
         return x
